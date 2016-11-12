d3713a86d5b1450bb69bdca74080a9afd66bb3ee
ulgoon <ulgoon89@gmail.com>
2016-11-12 23:57:14 +0900

    add uikit and layout complete

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var indexRouter = require('./routes/index')(app);
 var aboutRouter = require('./routes/about')(app);
 var shopRouter = require('./routes/shop')(app);
 
+var pug = require('pug');
+var path = require('path');
 
 app.set('view engines', 'pug');
 app.set('views', __dirname + '/views');
+
+app.use('/public', express.static(path.join(__dirname + '/public')));
 
 app.set('port', process.env.PORT||3030);
 
